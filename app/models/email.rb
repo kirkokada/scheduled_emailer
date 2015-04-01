85b70b5c14c0095bb608af40fc8c24d73930d60a
Kirk Okada <kirkokada@gmail.com>
2015-03-31 21:14:40 -1000

    Add Email.to address validation

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,8 +1,16 @@
 class Email < ActiveRecord::Base
   validate :deliver_at_is_valid_date_time
   validate :deliver_at_is_in_the_future
+  validate :email_is_valid
+  validates :to, presence: true
 
   private
+
+    def email_is_valid
+      mg = Mailgun::Client.new(ENV['mailgun_api_key'])
+      res = mg.get("address/validate", { address: to }).to_h
+      errors.add(:to, "must be a valid email address") unless res["is_valid"]
+    end
 
     def deliver_at_is_valid_date_time
       if ((DateTime.parse(deliver_at.to_s) rescue ArgumentError) == ArgumentError)
--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Email, type: :model do
-  subject { Email.new }
+  subject { Email.new(to: "user@email.com", deliver_at: 3.days.from_now) }
 
   it { should respond_to :to }
   it { should respond_to :subject }
@@ -11,18 +11,48 @@ RSpec.describe Email, type: :model do
   context "when deliver_at" do
     
     describe "is not a datetime" do
+
       before { subject.deliver_at = "March 2011"}
+
       it { should_not be_valid }
     end
-    
+
     describe "is not a future datetime" do
+
       before { subject.deliver_at = 3.hours.ago }
+
       it { should_not be_valid }
     end
 
     describe "is a future datetime" do
+
       before { subject.deliver_at = 3.hours.from_now }
+
       it { should be_valid }
+    end
+  end
+
+  context "when to" do
+    
+    describe "is nil" do
+      
+      before { subject.to = nil }
+
+      it { should_not be_valid }
+    end
+
+    describe "is blank" do
+      
+      before { subject.to = " " }
+
+      it { should_not be_valid }
+    end
+
+    describe "is not a valid email" do
+      
+      before { subject.to = "not@nemail,crom" }
+
+      it { should_not be_valid }
     end
   end
 end
