a790fb22e15906a54624fed9eae90780bc4e7928
Kirk Okada <kirkokada@gmail.com>
2015-04-01 10:35:32 -1000

    Add emails#new,create

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -4,5 +4,20 @@ class EmailsController < ApplicationController
   end
 
   def new
+    @email = Email.new
   end
+
+  def create
+    @email = Email.new(email_params)
+    if @email.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+    def email_params
+      params.require(:email).permit(:to, :subject, :body, :deliver_at)
+    end
 end
--- spec/requests/email_pages_spec.rb
+++ spec/requests/email_pages_spec.rb
@@ -3,6 +3,78 @@ require 'support/factory_girl'
 
 RSpec.describe "Email Pages", type: :feature do
   subject { page }
+
+  describe "new" do
+    
+    before { visit new_email_path }
+
+    describe "page" do
+
+      it { should have_selector "h1", text: "New Email" }
+      it { should have_selector "input#email_to" }
+      it { should have_selector "input#email_subject" }
+      it { should have_selector "textarea#email_body" }
+      it { should have_button   "Schedule Email" }
+      
+      it "should have year, month, day, hour, and minute selectors" do
+        5.times do |n|
+          n += 1
+          expect(page).to have_selector "select#email_deliver_at_#{n}i"
+        end
+      end
+    end
+
+    context "with invalid information" do
+      before do
+        fill_in "To", with: "not_an_email_address"
+        select "2010", from: "email_deliver_at_1i"
+      end
+
+      it "should not create a scheduled email" do
+        expect do 
+          click_button "Schedule Email" 
+        end.not_to change(Email, :count)
+      end
+
+      context "after submitting the form" do
+        before { click_button "Schedule Email" }
+
+        it "should render email#new" do
+          expect(page).to have_selector "h1", text: "New Email"
+        end
+
+        it { should have_selector "div#error_messages" }
+      end
+    end
+
+    context "with valid information" do
+      let(:subject_text) { "Test Email Subject" }
+      before do
+        fill_in "To", with: "testemail@gmail.com"
+        fill_in "Subject", with: subject_text
+        fill_in "Body", with: "Hello"
+        select "2020", from: "email_deliver_at_1i"
+      end
+      
+      it "should create a scheduled email" do
+        expect do 
+          click_button "Schedule Email" 
+        end.to change(Email, :count)
+      end
+
+      context "after submitting the form" do
+        before { click_button "Schedule Email" }
+
+        it "should redirect to root" do
+          expect(page).to have_selector "h1", text: "Scheduled Emails"
+        end
+
+        it "should list the scheduled email" do
+          expect(page).to have_content subject_text
+        end
+      end
+    end
+  end
   
   describe "index" do
 
