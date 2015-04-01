a91de5d32bccf58e34a8ee835d421530de9731bb
Kirk Okada <kirkokada@gmail.com>
2015-03-31 17:06:59 -1000

    Email.deliver_at tests

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,18 @@
 class Email < ActiveRecord::Base
+  validate :deliver_at_is_valid_date_time
+  validate :deliver_at_is_in_the_future
+
+  private
+
+    def deliver_at_is_valid_date_time
+      if ((DateTime.parse(deliver_at.to_s) rescue ArgumentError) == ArgumentError)
+        errors.add(:deliver_at, 'must be a valid datetime')
+      end
+    end
+
+    def deliver_at_is_in_the_future
+      if DateTime.now > DateTime.parse(deliver_at.to_s)
+        errors.add(:deliver_at, "must be a future datetime")
+      end
+    end
 end
--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -1,8 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Email, type: :model do
+  subject { Email.new }
+
   it { should respond_to :to }
   it { should respond_to :subject }
   it { should respond_to :body }
   it { should respond_to :deliver_at }
+
+  context "when deliver_at" do
+    
+    describe "is not a datetime" do
+      before { subject.deliver_at = "March 2011"}
+      it { should_not be_valid }
+    end
+    
+    describe "is not a future datetime" do
+      before { subject.deliver_at = 3.hours.ago }
+      it { should_not be_valid }
+    end
+
+    describe "is a future datetime" do
+      before { subject.deliver_at = 3.hours.from_now }
+      it { should be_valid }
+    end
+  end
 end
